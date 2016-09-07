78b0ce35ec43c4536e99caa4074a896ea25e99d4
Josh Granek <josh@Joshs-MBP.dhe.duke.edu>
2016-09-07 15:13:09 -0400

    fill_in_metadata.R: seems to be working

--- fill_in_metadata.R
+++ fill_in_metadata.R
@@ -13,19 +13,15 @@ sample_key <- read.csv("metadata_key.csv", header=TRUE)
 sep_id = df %>% rename(SampleID=X.SampleID) %>%
   extract(col=SampleID,into=c("group","animal"),regex="([[:alpha:]]+)([[:digit:]]+)",remove = FALSE)
 
-
-
-
-
 sep_id %>% mutate(antibiotic = mapvalues(group, 
                                          c("NLU",  "ASNLU", "AINLU", "APNLU"), 
                                          c("none", "subq", "iv", "oral"))) 
                     
                     
-                    SampleID)
+#                     SampleID)
   
   
-  plyr::mapvaluesx <- c("a", "b", "c")
+plyr::mapvaluesx <- c("a", "b", "c")
 mapvalues(x, c("a", "c"), c("A", "C"))
 
 list(c("a",1),c("b",2))
