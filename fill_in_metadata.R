a9e16661076c87af0e6b60739f150dfac30d8355
Josh Granek <josh@Joshs-MBP.dhe.duke.edu>
2016-09-07 15:38:35 -0400

    fill_in_metadata.R: Now saves generated map to file.  Also did some cleanup.

--- fill_in_metadata.R
+++ fill_in_metadata.R
@@ -1,28 +1,27 @@
 library(tidyr)
 library(plyr); library(dplyr)
 
-# Load Sample File
-f <- file("notes_and_info/160613_McKenney_MF_NJK_06012016_16SRV.TXT", open="r", encoding="UTF-16LE")
+# Set up directory and files
+info_dir = "notes_and_info"
+raw_sample_map = file.path(info_dir, "160613_McKenney_MF_NJK_06012016_16SRV.TXT")
+raw_sample_key = file.path(info_dir, "metadata_key.csv")
+final_sample_map = file.path(info_dir, "rat_lung_map.tsv")
+
+# need to jump through some hoops to open file with UTF-16LE encoding
+f <- file(raw_sample_map, open="r", encoding="UTF-16LE")
 df <- read.table(f, sep='\t', dec=',', header=TRUE,comment="")
+close(f)
 
 # Load Sample Key
-sample_key <- read.csv("notes_and_info/metadata_key.csv", header=TRUE)
-
+sample_key <- read.csv(raw_sample_key, header=TRUE)
 
 # Split sample ID
 sep_id = df %>% rename(SampleID=X.SampleID) %>%
   extract(col=SampleID,into=c("group","animal"),regex="([[:alpha:]]+)([[:digit:]]+)",remove = FALSE)
 
-sep_id %>% mutate(antibiotic = mapvalues(group, 
+map_df = sep_id %>% mutate(antibiotic = mapvalues(group, 
                                          c("NLU",  "ASNLU", "AINLU", "APNLU"), 
-                                         c("none", "subq", "iv", "oral"))) 
+                                         c("none", "subq", "iv", "oral")))
+write.table(map_df,final_sample_map,quote=FALSE,row.names = FALSE)
                     
-                    
-#                     SampleID)
-  
-  
-plyr::mapvaluesx <- c("a", "b", "c")
-mapvalues(x, c("a", "c"), c("A", "C"))
-
-list(c("a",1),c("b",2))
-unique(sep_id$group)
+# unique(sep_id$group)
