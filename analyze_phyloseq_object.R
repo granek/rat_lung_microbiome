7f33d86231c40aad7f1ce4538eb839516dcebd5d
Josh Granek <josh@duke.edu>
2016-09-14 11:19:39 -0400

    analyze_phyloseq_object.R: did some cleanup

--- analyze_phyloseq_object.R
+++ analyze_phyloseq_object.R
@@ -3,11 +3,6 @@
 
 suppressPackageStartupMessages(library("argparse"))
 parser <- ArgumentParser()
-parser$add_argument("-f", "--filter_fastqs", action="store_true", default=FALSE,
-                    help="Run filtering step (default is to assume it was already run)")
-parser$add_argument("-q", "--quality_plots", type="integer", default=0,
-                    help="Number of quality plots generate [default %(default)s]",
-                    metavar="number")
 parser$add_argument("--basedir", default=".",
                     help="Base directory for analysis [default %(default)s]",
                     metavar="DIR")
@@ -15,27 +10,19 @@ args <- parser$parse_args()
 #--------------------------------------------------
 basedir = args$basedir
 
-map_file = file.path(basedir, "notes_and_info/rat_lung_map.tsv")
 workdir = file.path(basedir, "workspace")
-
-taxonomy_dir =file.path(workdir,"taxonomy_refs")
-silva_ref = file.path(taxonomy_dir,"silva_nr_v123_train_set.fa.gz")
-
 results_dir = file.path(workdir,"results")
 psfile.prefix = file.path(results_dir, "rat_lung_ps")
 
 ##====================================================================
 ##====================================================================
 #+ Setup: Load Libraries, include=FALSE
-## print(.libPaths())
-# library(dada2)
-# library(ShortRead)
 library(ggplot2)
 library(phyloseq)
 library(dplyr)
-# library(biom)
+library(DESeq2)
 writeLines(capture.output(sessionInfo()), file.path(results_dir,"analyze_phyloseq_object_sessionInfo.txt"))
-##====================================================================
+# ##====================================================================
 loadPhyloseqFiles = function(otu_table_file,sample_data_file,tax_table_file){
   otab <- otu_table(read.csv(otu_table_file,row.names=1), taxa_are_rows=FALSE)
   taxtab <- tax_table(as.matrix(read.csv(tax_table_file,row.names=1)))
@@ -49,13 +36,14 @@ loadPhyloseqFiles = function(otu_table_file,sample_data_file,tax_table_file){
 }
 #==============================================================================
 list.files(results_dir)
-setwd("./parker_rat_lung/")
+# setwd("./parker_rat_lung/")
 # output.files = outputPhyloseq(ps,seqid.map.df,psfile.prefix)
 otu_table_file = paste0(psfile.prefix,"_otu.csv")
 sample_data_file = paste0(psfile.prefix,"_samdat.csv")
 tax_table_file = paste0(psfile.prefix,"_tax.csv")
 
 ps = loadPhyloseqFiles(otu_table_file,sample_data_file,tax_table_file)
+# ps = readRDS(paste0(psfile.prefix,".rds"))
 
 plot_richness(ps, x="animal", measures=c("Shannon", "Simpson"), color="antibiotic") + theme_bw()
 plot_bar(ps, x="antibiotic", fill="Family") 
@@ -113,3 +101,4 @@ ggplot(min_max_counts, aes(x=Description,ymin = `min`, ymax = `max`,color=group)
         axis.text.x=element_blank(),
         panel.background = element_blank())
 
+print(paste("Lowest Maximum Value:", min(min_max_counts$max)))
