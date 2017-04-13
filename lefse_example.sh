c5b881b729348d7dfde47a09baa540c470e279a3
Rapid User <josh@duke.edu>
2017-04-13 16:24:21 +0000

    lefse_example.sh: merged info from lefse_notes.txt

--- lefse_example.sh
+++ lefse_example.sh
@@ -1,3 +1,5 @@
+
+
 # https://bitbucket.org/biobakery/biobakery/wiki/lefse#rst-header-lefse-bitbucket
 mkdir -p /home/rstudio/parker_rat_lung/workspace/lefse_workspace/
 cd /home/rstudio/parker_rat_lung/workspace/lefse_workspace/
@@ -11,3 +13,7 @@ run_lefse.py tmp/hmp_aerobiosis_small.in results/hmp_aerobiosis_small.res
 
 lefse-plot_res.py results/hmp_aerobiosis_small.res output_figures/hmp_aerobiosis_small.png
 lefse-plot_cladogram.py results/hmp_aerobiosis_small.res output_figures/hmp_aerobiosis_small.cladogram.png --format png
+
+# Also see
+# https://bitbucket.org/nsegata/lefse/src/54694b4b0d9e335ff1ecafff8db4f1e0cf7004da?at=default
+# https://bitbucket.org/nsegata/metaphlan/wiki/MetaPhlAn_Pipelines_Tutorial (see Step 4: Taxonomic biomarker discovery with LEfSe)
