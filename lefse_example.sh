d2306bf7dda91fdae3ae5812d6443ef547d18a49
Rapid User <josh@duke.edu>
2017-04-13 15:47:13 +0000

    lefse_example.sh: first commit!  Works!

---
+++ lefse_example.sh
@@ -0,0 +1,13 @@
+# https://bitbucket.org/biobakery/biobakery/wiki/lefse#rst-header-lefse-bitbucket
+mkdir -p /home/rstudio/parker_rat_lung/workspace/lefse_workspace/
+cd /home/rstudio/parker_rat_lung/workspace/lefse_workspace/
+wget http://huttenhower.sph.harvard.edu/webfm_send/129
+mkdir -p input tmp results output_figures
+mv 129 input/hmp_aerobiosis_small.txt
+
+source /opt/conda/bin/activate qiime1
+lefse-format_input.py input/hmp_aerobiosis_small.txt tmp/hmp_aerobiosis_small.in -c 1 -s 2 -u 3 -o 1000000
+run_lefse.py tmp/hmp_aerobiosis_small.in results/hmp_aerobiosis_small.res
+
+lefse-plot_res.py results/hmp_aerobiosis_small.res output_figures/hmp_aerobiosis_small.png
+lefse-plot_cladogram.py results/hmp_aerobiosis_small.res output_figures/hmp_aerobiosis_small.cladogram.png --format png
