d9d3d75d66b6f74472bce6920cf10b3627c079b7
Rapid User <josh@duke.edu>
2017-04-19 19:40:38 +0000

    run_lefse.sh: updated to run on antibiotic bool and factor

--- run_lefse.sh
+++ run_lefse.sh
@@ -4,18 +4,11 @@
 
 source /opt/conda/bin/activate qiime1
 
-# INPUT_FILE="workspace/lefse/antibiotic_wcontrol.tsv"
-# LEFSE_INPUT="workspace/lefse/antibiotic_wcontrol.in"
-# LEFSE_REFORMATTED_TABLE="workspace/lefse/antibiotic_wcontrol.tab"
-# LEFSE_RESULTS="workspace/lefse/antibiotic_wcontrol.res"
-# LEFSE_PLOT="workspace/lefse/antibiotic_wcontrol.pdf"
+BOOL_INPUT="workspace/lefse/antibiotic_bool.tsv"
+FACTOR_INPUT="workspace/lefse/antibiotic_factor.tsv"
 
-INPUT_FILE=${1:-"workspace/lefse/antibiotic_factor.tsv"}
+# INPUT_FILE=${1:-"workspace/lefse/antibiotic_factor.tsv"}
 BASENAME="${INPUT_FILE%.*}"
-LEFSE_INPUT="${BASENAME}.in"
-LEFSE_REFORMATTED_TABLE="${BASENAME}.tab"
-LEFSE_RESULTS="${BASENAME}.res"
-LEFSE_PLOT="${BASENAME}.pdf"
 
 # Running the LEfSe commands with -h gives the list of available options
 
@@ -28,42 +21,55 @@ LEFSE_PLOT="${BASENAME}.pdf"
 # -o 1000000 scales the feature such that the sum (of the same taxonomic leve)
 # is 1M: this is done only for obtaining more meaningful values for the LDA score
 
-# lefse-format_input.py $INPUT_FILE  $LEFSE_INPUT -f c -u 1 -c 2 -s 3 -o 1000000  --output_table $LEFSE_REFORMATTED_TABLE
-lefse-format_input.py $INPUT_FILE  $LEFSE_INPUT -f c -u 1 -c 2 -o 1000000  --output_table $LEFSE_REFORMATTED_TABLE
+# lefse-format_input.py $INPUT_FILE  "${BASENAME}.in" -f c -u 1 -c 2 -s 3 -o 1000000  --output_table ${BASENAME}.tab
 
-# run_lefse.py performs the actual statistica analysis
-#
-# Apply LEfSe on the formatted data producing the results (to be further processed
-# for visualization with the other modules). The option available
-# can be listed using the -h option 
-run_lefse.py $LEFSE_INPUT $LEFSE_RESULTS
+INPUT_FILE=$BOOL_INPUT
+BASENAME="${INPUT_FILE%.*}"
+lefse-format_input.py $INPUT_FILE  "${BASENAME}.in" -f c -u 1 -c 2 -s 3 -o 1000000  --output_table ${BASENAME}.tab
+
+INPUT_FILE=$FACTOR_INPUT
+BASENAME="${INPUT_FILE%.*}"
+lefse-format_input.py $INPUT_FILE  "${BASENAME}.in" -f c -u 1 -c 2 -o 1000000  --output_table ${BASENAME}.tab
+
+for INPUT_FILE in $BOOL_INPUT $FACTOR_INPUT; do
+    BASENAME="${INPUT_FILE%.*}"
+    # BASENAME=
+    # BASENAME=$(basename "${INPUT_FILE}")
+    # DIRNAME=$(dirname "${INPUT_FILE}")
+    # run_lefse.py performs the actual statistica analysis
+    #
+    # Apply LEfSe on the formatted data producing the results (to be further processed
+    # for visualization with the other modules). The option available
+    # can be listed using the -h option 
+    run_lefse.py "${BASENAME}.in" ${BASENAME}.res
+
+    # plot_res.py visualizes the output
+    #
+    # Plot the list of biomarkers with their effect size
+    # Severak graphical options are available for personalizing the output
+    # ../plot_res.py hmp_aerobiosis_small.res hmp_aerobiosis_small.png
+    lefse-plot_res.py --format pdf ${BASENAME}.res ${BASENAME}.pdf
+    
+    # plot_cladogram.py visualizes the output on a hierarchical tree
+    #
+    # Plot the representation of the biomarkers on the hierarchical tree
+    # specified in the input data (using | in the name of the features)
+    # In this case we will obtain the RDP taxonomy.
+    # This is an early implementation of the module. I'm working on an improved version
+    # that will be released independently from LEfSe
+    lefse-plot_cladogram.py ${BASENAME}.res ${BASENAME}.cladogram.pdf --format pdf
+    
+    # Create a directory for storing the raw-data representation of the discovered biomarkers
+    mkdir -p ${BASENAME}/biomarkers_raw_images
+    
+    # plot_features.py visualizes the raw-data features
+    #
+    # The module for exporting the raw-data representation of the features.
+    # With the default options we will obtain the images for all the features that are
+    # detected as biomarkers
+    lefse-plot_features.py ${BASENAME}.in ${BASENAME}.res ${BASENAME}/biomarkers_raw_images/
+done
 
 
-# plot_res.py visualizes the output
-#
-# Plot the list of biomarkers with their effect size
-# Severak graphical options are available for personalizing the output
-# ../plot_res.py hmp_aerobiosis_small.res hmp_aerobiosis_small.png
-lefse-plot_res.py --format pdf $LEFSE_RESULTS $LEFSE_PLOT
-
-
-# plot_cladogram.py visualizes the output on a hierarchical tree
-#
-# Plot the representation of the biomarkers on the hierarchical tree
-# specified in the input data (using | in the name of the features)
-# In this case we will obtain the RDP taxonomy.
-# This is an early implementation of the module. I'm working on an improved version
-# that will be released independently from LEfSe
-# ../plot_cladogram.py hmp_aerobiosis_small.res hmp_aerobiosis_small.cladogram.png --format png
-
-# Create a directory for storing the raw-data representation of the discovered biomarkers
-# mkdir biomarkers_raw_images
-
-# plot_features.py visualizes the raw-data features
-#
-# The module for exporting the raw-data representation of the features.
-# With the default options we will obtain the images for all the features that are
-# detected as biomarkers
-# ../plot_features.py hmp_aerobiosis_small.in hmp_aerobiosis_small.res biomarkers_raw_images/
 
 
