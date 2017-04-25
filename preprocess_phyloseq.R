893419c8f2f7e6fe7a7add65ef6f64f794329aad
Rapid User <josh@duke.edu>
2017-04-25 04:25:14 +0000

    preprocess_phyloseq.R: cleaned up cruft

--- preprocess_phyloseq.R
+++ preprocess_phyloseq.R
@@ -24,14 +24,8 @@ phyloseq_rds.filename = file.path("results", "rat_lung_ps.rds")
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #+ Setup: Load Libraries, include=FALSE
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(phyloseq))
 suppressPackageStartupMessages(library(dplyr))
-# suppressPackageStartupMessages(library(magrittr))
-# suppressPackageStartupMessages(library("tibble"))
-# suppressPackageStartupMessages(library(DESeq2))
-# suppressPackageStartupMessages(library(vegan))
-# suppressPackageStartupMessages(library(gridExtra))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #+ Setup: Load Phyloseq object from RDS, include=FALSE
@@ -45,14 +39,8 @@ sample_data(full_ps)$antibiotic = relevel(sample_data(full_ps)$antibiotic, "none
 sample_data(full_ps)$left_aspiration = relevel(sample_data(full_ps)$left_aspiration, "none")
 sample_data(full_ps)$right_aspiration = relevel(sample_data(full_ps)$right_aspiration, "none")
 sample_data(full_ps)$sample_aspiration = relevel(sample_data(full_ps)$sample_aspiration, "none")
-levels(sample_data(full_ps)$antibiotic)
-levels(sample_data(full_ps)$left_aspiration)
-levels(sample_data(full_ps)$right_aspiration)
-levels(sample_data(full_ps)$sample_aspiration)
 
-# antibiotic_bool = 
 sample_data(full_ps)$antibiotic_bool <- factor(get_variable(full_ps, "antibiotic") != "none")
-# aspiration_bool = 
 sample_data(full_ps)$aspiration_bool <- factor(get_variable(full_ps, "sample_aspiration") != "none")
 
 saveRDS(full_ps, file.path(workdir, "full_relevelled_ps.rds"))
@@ -61,18 +49,8 @@ saveRDS(full_ps, file.path(workdir, "full_relevelled_ps.rds"))
 #+ Extract subset of replicates with most counts in each pair, include=FALSE
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 bacteria_ps_both_reps = subset_taxa(full_ps,Kingdom=="Bacteria") # Drop non-bacteria taxa
-# total_count.df = data.frame(sample_data(full_ps), total_count=sample_sums(full_ps))
 total_count.df = data.frame(sample_data(bacteria_ps_both_reps), total_count=sample_sums(bacteria_ps_both_reps))
-#   
-#   
-#   
-#   as.data.frame(rowSums(otu_table(full_ps)))
-# total_count.df = as.data.frame(rowSums(otu_table(full_ps)))
-# mysums = sample_sums(full_ps)
-# sum.df <- 
-# colnames(sum.df)[length(colnames(sum.df))] <- "depth"
-# 
-# colnames(total_counts) = "totals"
+
 set.seed(1)
 max_replicate = total_count.df %>% 
   select(SampleID, Description, group, total_count) %>%
@@ -100,5 +78,5 @@ saveRDS(antibiotic_ps, file.path(workdir, "antibiotic_ps.rds"))
 #+ Session Info, echo=FALSE
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 sessionInfo()
-writeLines(capture.output(sessionInfo()), file.path(results_dir,"analyze_phyloseq_object_sessionInfo.txt"))
+# writeLines(capture.output(sessionInfo()), file.path(results_dir,"analyze_phyloseq_object_sessionInfo.txt"))
 
